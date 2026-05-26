7386b275b443032c37cfd09abd21bb4b49575626
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-05-26 12:10:11 +0530

    implement syscall5 and syscall6

--- include/syscall.h
+++ include/syscall.h
@@ -68,4 +68,37 @@ static inline long syscall4(long syscallNum, long rdi, long rsi, long rdx, long 
     return ret;
 }
 
+static inline long syscall5(long syscallNum, long rdi, long rsi, long rdx, long r10, long r8)
+{
+    long ret;
+
+    register long r10val asm("r10") = r10;
+    register long r8val asm("r8") = r8;
+
+    asm volatile(
+        "syscall"
+        : "=a"(ret)
+        : "0"(syscallNum), "D"(rdi), "S"(rsi), "d"(rdx), "r"(r10val), "r"(r8val)
+        : "rcx", "r11", "memory");
+
+    return ret;
+}
+
+static inline long syscall6(long syscallNum, long rdi, long rsi, long rdx, long r10, long r8, long r9)
+{
+    long ret;
+
+    register long r10val asm("r10") = r10;
+    register long r8val asm("r8") = r8;
+    register long r9val asm("r9") = r9;
+
+    asm volatile(
+        "syscall"
+        : "=a"(ret)
+        : "0"(syscallNum), "D"(rdi), "S"(rsi), "d"(rdx), "r"(r10val), "r"(r8val), "r"(r9val)
+        : "rcx", "r11", "memory");
+
+    return ret;
+}
+
 #endif /* SYSCALL_H_ */
